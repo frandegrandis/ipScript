b13d6715036fe894de904c9bf2d34669128e818f
Francisco De Grandis <frandegrandis@gmail.com>
2021-05-05 15:22:11 -0300

    finished Table sorting on click of headers

--- ipTable.py
+++ ipTable.py
@@ -4,19 +4,19 @@ import tksheet
 
 class ipTable:
 
-    def __init__(self,data):
+    def __init__(self, data):
         top = tk.Tk()
-        headers = ["IP", "Location", "Is Proxy","Tor Node", "ASM"]
-        sheet = tksheet.Sheet(top, headers=headers)
-        sheet.grid()
-        sheet.set_sheet_data(data)
-        sheet.enable_bindings(("single_select",
-                               "row_select",
-                               "column_width_resize",
-                               "arrowkeys",
-                               "right_click_popup_menu",
-                               "rc_select",
-                               "copy"))
-        sheet.pack(expand=1, fill=tk.BOTH)
+        headers = ["IP", "Location", "Is Proxy", "Tor Node", "ASM"]
+        self.data = data
+        self.sheet = tksheet.Sheet(top, headers=headers)
+        self.sheet.grid()
+        self.sheet.set_sheet_data(data)
+        self.sheet.enable_bindings()
+        self.sheet.extra_bindings("column_select", func=self.sortTable)
+        self.sheet.pack(expand=1, fill=tk.BOTH)
         top.mainloop()
 
+    def sortTable(self, event=None):
+        # Event es una tupla del estilo (evento,columna_seleccionada)
+        self.data = sorted(self.data, key=lambda x: x[event[1]])
+        self.sheet.set_sheet_data(self.data)
